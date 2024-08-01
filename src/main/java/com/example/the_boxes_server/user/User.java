a26aaef04ca3922620f777730cac46af145da9cd
vosw1 <vosw1@naver.com>
2024-08-01 10:26:11 +0900

    user패키지 및 테이블 생성

---
+++ src/main/java/com/example/the_boxes_server/user/User.java
@@ -0,0 +1,81 @@
+package com.example.the_boxes_server.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId; // 사용자 ID (기본 키)
+
+    private String profilePicture; // 프로필 사진 경로
+
+    @Column(unique = true, length = 50, nullable = false)
+    private String username; // 사용자 아이디
+
+    @Column(length = 20, nullable = false)
+    private String password; // 비밀번호
+
+    @Column(length = 20, nullable = false)
+    private String name; // 이름
+
+    private Date birthdate; // 생년월일
+    private String phone; // 전화번호
+    private String address; // 주소
+
+    @Column(length = 100, nullable = false)
+    private String email; // 이메일
+
+    private String companyAddress; // 회사 주소
+    private String industry; // 업종
+    private String position; // 직책
+    private String logisticsCenterLocation; // 물류 센터 위치
+    private String equipment; // 보유 장비
+
+    @CreationTimestamp
+    private Timestamp createdAt; // 생성 시간
+
+    @Builder
+    public User(
+            Integer userId,
+            String profilePicture,
+            String username,
+            String password,
+            String name,
+            Date birthdate,
+            String phone,
+            String address,
+            String email,
+            String companyAddress,
+            String industry,
+            String position,
+            String logisticsCenterLocation,
+            String equipment
+    ) {
+        this.userId = userId;
+        this.profilePicture = profilePicture;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+        this.companyAddress = companyAddress;
+        this.industry = industry;
+        this.position = position;
+        this.logisticsCenterLocation = logisticsCenterLocation;
+        this.equipment = equipment;
+    }
+}
---
+++ src/main/java/com/example/the_boxes_server/user/UserService.java
@@ -0,0 +1,4 @@
+package com.example.the_boxes_server.user;
+
+public class UserService {
+}
