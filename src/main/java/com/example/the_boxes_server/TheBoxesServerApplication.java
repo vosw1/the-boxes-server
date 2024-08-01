faf8598c172c33c8843edbf4de1aefe2e27188fb
vosw1 <vosw1@naver.com>
2024-08-01 10:16:06 +0900

    처음 셋팅

---
+++ src/main/java/com/example/the_boxes_server/TheBoxesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_boxes_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBoxesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheBoxesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/the_boxes_server/TheBoxesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_boxes_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBoxesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
