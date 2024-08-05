a8f97ba7a7eabde207753801fa66c71fece15192
vosw1 <vosw1@naver.com>
2024-08-05 21:02:37 +0900

    user 더미 데이터 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,17 +1,21 @@
-INSERT INTO users (
-    username, password, name, address, email, birthdate, phone,
-    company_address, industry, logistics_center_location, position, equipment
-) VALUES (
-             'asdf',
-             'as1234',
-             '홍길동',
-             '부산광역시 부산진구 가야동',
-             'as1234@nate.com',
-             '1990-05-15',
-             '010-1234-7890',
-             'Tech Solutions',
-             '물류',
-             '부산광역시 강서구',
-             '사원',
-             '바코드'
-         );
+INSERT INTO users (username, password, name, address, email, birthdate, phone,
+                   company_address, industry, logistics_center_location, position, equipment, created_at)
+VALUES (
+        'asdf',
+        'as1234',
+        '홍길동',
+        '부산광역시 부산진구 가야동',
+        'as1234@nate.com',
+        '1990-05-15',
+        '010-1234-7890',
+        'Tech Solutions',
+        '물류',
+        '부산광역시 강서구',
+        '사원',
+        '바코드',
+        '2024-07-01'
+       );
+
+
+
+
